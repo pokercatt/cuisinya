070bc2a3a88e6d78d3f476f0ce347c8cd3d7f33e
Tim <timnar@icloud.com>
2022-04-02 13:56:31 +0200

    site identity reimagined

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '4ec98491-9e30-43e5-9cd0-7d2b800b6cae';
+export const MODAL_CLOSE_SEC = 2.5;
